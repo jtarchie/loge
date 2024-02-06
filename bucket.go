4f4b28afc55705628d9d905439c73579a808f5a8
JT Archie <jtarchie@gmail.com>
2024-02-06 15:58:49 -0700

    refactor buckets

---
+++ bucket.go
@@ -0,0 +1,147 @@
+package loge
+
+import (
+	"database/sql"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Bucket struct {
+	payload     []*Payload
+	payloadSize int
+	outputDir   string
+}
+
+type Buckets []*Bucket
+
+func NewBuckets(
+	size int,
+	payloadSize int,
+	outputPath string,
+) Buckets {
+	var buckets Buckets
+
+	for i := 0; i < size; i++ {
+		buckets = append(buckets, NewBucket(payloadSize, outputPath))
+	}
+
+	return buckets
+}
+
+func (b Buckets) Append(index int, payload *Payload) error {
+	err := b[index].Append(payload)
+	if err != nil {
+		return fmt.Errorf("could not append to bucket %d: %w", index, err)
+	}
+
+	return nil
+}
+
+func NewBucket(
+	size int,
+	outputDir string,
+) *Bucket {
+	return &Bucket{
+		outputDir:   outputDir,
+		payload:     make([]*Payload, 0, size),
+		payloadSize: size,
+	}
+}
+
+func (b *Bucket) Append(payload *Payload) error {
+	b.payload = append(b.payload, payload)
+
+	if b.payloadSize <= len(b.payload) {
+		err := b.flush()
+		if err != nil {
+			return fmt.Errorf("could not flush: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (b *Bucket) flush() error {
+	filename := filepath.Join(b.outputDir, fmt.Sprintf("%d.sqlite", time.Now().UnixNano()))
+
+	client, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return fmt.Errorf("could not open sqlite3 %q: %w", filename, err)
+	}
+	defer client.Close()
+
+	client.Exec(`
+		CREATE TABLE labels (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			payload JSONB
+		);
+
+		CREATE TABLE streams (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			timestamp INTEGER,
+			line TEXT,
+			label_id INTEGER
+		);
+	`)
+
+	transaction, err := client.Begin()
+	if err != nil {
+		return fmt.Errorf("could not create transaction %q: %w", filename, err)
+	}
+	defer transaction.Rollback()
+
+	insertStream, err := transaction.Prepare(`
+		INSERT INTO streams
+			(timestamp, line, label_id)
+				VALUES
+			(?, ?, ?);
+	`)
+	if err != nil {
+		return fmt.Errorf("could not prepare insert %q: %w", filename, err)
+	}
+
+	insertLabels, err := transaction.Prepare(`
+		INSERT INTO labels
+			(payload)
+				VALUES
+			(?);
+	`)
+	if err != nil {
+		return fmt.Errorf("could not prepare insert %q: %w", filename, err)
+	}
+
+	for _, payload := range b.payload {
+		for _, stream := range payload.Streams {
+			resultLabel, err := insertLabels.Exec(MarshalLabels(stream.Stream))
+			if err != nil {
+				return fmt.Errorf("could not insert %q: %w", filename, err)
+			}
+
+			labelID, _ := resultLabel.LastInsertId()
+
+			for _, value := range stream.Values {
+				_, err := insertStream.Exec(value[0], value[1], labelID)
+				if err != nil {
+					return fmt.Errorf("could not insert %q: %w", filename, err)
+				}
+			}
+		}
+	}
+
+	err = transaction.Commit()
+	if err != nil {
+		return fmt.Errorf("could not commit transaction %q: %w", filename, err)
+	}
+
+	client.Exec(`
+		vacuum;
+		pragma optimize;
+	`)
+
+	b.payload = b.payload[:0]
+
+	return nil
+}
--- cli.go
+++ cli.go
@@ -1,171 +1,34 @@
 package loge
 
 import (
-	"database/sql"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/goccy/go-json"
 	"github.com/jtarchie/worker"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	_ "github.com/mattn/go-sqlite3"
 	slogecho "github.com/samber/slog-echo"
 	"github.com/tinylib/msgp/msgp"
 )
 
 type CLI struct {
-	Port       int `help:"start HTTP server on port" default:"3000" required:""`
-	Buckets    int `help:"number of buckets to fill into" required:"" default:"8"`
-	BucketSize int `help:"size of the bucket" required:"" default:"100000"`
+	Port        int    `help:"start HTTP server on port" default:"3000" required:""`
+	Buckets     int    `help:"number of buckets to fill into" required:"" default:"8"`
+	PayloadSize int    `help:"size of the bucket payload" required:"" default:"10000"`
+	OutputPath  string `help:"output path for all the sqlite files" required:"" default:"tmp/"`
 }
 
 func (c *CLI) Run() error {
+	buckets := NewBuckets(c.Buckets, c.PayloadSize, c.OutputPath)
 
-	buckets := make([][]*Payload, c.Buckets)
-	for i := 0; i < c.Buckets; i++ {
-		buckets[i] = make([]*Payload, 0, c.BucketSize)
-	}
-
-	bucketWorkers := worker.New(c.BucketSize, c.Buckets, func(index int, payload *Payload) {
-		buckets[index-1] = append(buckets[index-1], payload)
-
-		// flush to sqlite
-		if c.BucketSize <= len(buckets[index-1]) {
-			defer func() {
-				buckets[index-1] = buckets[index-1][:0]
-			}()
-
-			filename := fmt.Sprintf("tmp/%d.sqlite", time.Now().UnixNano())
-
-			client, err := sql.Open("sqlite3", filename)
-			if err != nil {
-				slog.Error(
-					"could not open sqlite",
-					slog.String("error", err.Error()),
-					slog.String("filename", filename),
-				)
-
-				return
-			}
-			defer client.Close()
-
-			client.Exec(`
-					CREATE TABLE labels (
-						id INTEGER PRIMARY KEY AUTOINCREMENT,
-						payload JSONB
-					);
-
-					CREATE TABLE streams (
-						id INTEGER PRIMARY KEY AUTOINCREMENT,
-						timestamp INTEGER,
-						line TEXT,
-						label_id INTEGER
-					);
-				`)
-
-			transaction, err := client.Begin()
-			if err != nil {
-				slog.Error(
-					"could not create transaction",
-					slog.String("error", err.Error()),
-					slog.String("filename", filename),
-				)
-
-				os.Exit(1)
-
-				return
-			}
-			defer transaction.Rollback()
-
-			insertStream, err := transaction.Prepare(`
-				INSERT INTO streams
-					(timestamp, line, label_id)
-						VALUES
-					(?, ?, ?);
-				`)
-			if err != nil {
-				slog.Error(
-					"could not prepare insert",
-					slog.String("error", err.Error()),
-					slog.String("filename", filename),
-				)
-
-				os.Exit(1)
-
-				return
-			}
-
-			insertLabels, err := transaction.Prepare(`
-			INSERT INTO labels
-				(payload)
-					VALUES
-				(?);
-			`)
-			if err != nil {
-				slog.Error(
-					"could not prepare insert",
-					slog.String("error", err.Error()),
-					slog.String("filename", filename),
-				)
-
-				os.Exit(1)
-
-				return
-			}
-
-			for _, payload := range buckets[index-1] {
-				for _, stream := range payload.Streams {
-					resultLabel, err := insertLabels.Exec(MarshalLabels(stream.Stream))
-					if err != nil {
-						slog.Error(
-							"could not insert",
-							slog.String("error", err.Error()),
-							slog.String("filename", filename),
-						)
-
-						os.Exit(1)
-					}
-
-					labelID, _ := resultLabel.LastInsertId()
-
-					for _, value := range stream.Values {
-						_, err := insertStream.Exec(value[0], value[1], labelID)
-						if err != nil {
-							slog.Error(
-								"could not insert",
-								slog.String("error", err.Error()),
-								slog.String("filename", filename),
-							)
-
-							os.Exit(1)
-						}
-					}
-				}
-			}
-
-			err = transaction.Commit()
-			if err != nil {
-				slog.Error(
-					"could not commit",
-					slog.String("error", err.Error()),
-					slog.String("filename", filename),
-				)
-
-				os.Exit(1)
-			}
-
-			client.Exec(`
-				pragma journal_mode = delete; -- to be able to actually set page size
-				pragma page_size = 1024; -- trade off of number of requests that need to be made vs overhead. 
-
-				vacuum;
-				pragma optimize;
-			`)
+	bucketWorkers := worker.New(c.PayloadSize, c.Buckets, func(index int, payload *Payload) {
+		err := buckets.Append(index-1, payload)
+		if err != nil {
+			slog.Error("could not append to bucket", slog.Int("index", index), slog.String("error", err.Error()))
 		}
 	})
 

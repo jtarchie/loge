258eb9e68e761fdc9b7c2c86979133170f33affa
JT Archie <jtarchie@gmail.com>
2024-04-15 16:24:12 -0600

    refactor execute function

--- managers/local.go
+++ managers/local.go
@@ -22,48 +22,55 @@ func NewLocal(
 	}
 }
 
-func (m *Local) files() ([]string, error) {
+func (m *Local) execute(fun func(*sql.DB) error) error {
 	files, err := filepath.Glob(filepath.Join(m.outputDir, "*.sqlite.zst"))
 	if err != nil {
-		return nil, fmt.Errorf("could not load files: %w", err)
-	}
-
-	return files, nil
-}
-
-func (m *Local) Labels() ([]string, error) {
-	var foundLabels []string
-
-	files, err := m.files()
-	if err != nil {
-		return nil, fmt.Errorf("could not load sqlite: %w", err)
+		return fmt.Errorf("could not load files: %w", err)
 	}
 
 	for _, filename := range files {
 		client, err := sql.Open("sqlite3", filename+"?vfs=zstd")
 		if err != nil {
-			return nil, fmt.Errorf("could not open sqlite3 %q: %w", filename, err)
+			return fmt.Errorf("could not open sqlite3 %q: %w", filename, err)
 		}
 		defer client.Close()
 
+		err = fun(client)
+		if err != nil {
+			return fmt.Errorf("could not execute: %w", err)
+		}
+
+		err = client.Close()
+		if err != nil {
+			return fmt.Errorf("could not close connection: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (m *Local) Labels() ([]string, error) {
+	var foundLabels []string
+
+	err := m.execute(func(client *sql.DB) error {
 		var labels []string
 
-		err = sqlscan.Select(context.TODO(), client, &labels, `
+		err := sqlscan.Select(context.TODO(), client, &labels, `
 			SELECT
 				DISTINCT json_each.key
 			FROM labels,
 				json_each(labels.payload);
 		`)
 		if err != nil {
-			return nil, fmt.Errorf("could not scan labels: %w", err)
+			return fmt.Errorf("could not scan labels: %w", err)
 		}
 
 		foundLabels = append(foundLabels, labels...)
 
-		err = client.Close()
-		if err != nil {
-			return nil, fmt.Errorf("could not close connection: %w", err)
-		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not read all labels: %w", err)
 	}
 
 	return lo.Uniq(foundLabels), nil

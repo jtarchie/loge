455984f555b4d2c2eaf145fe9b6a38b86eba4f76
JT Archie <jtarchie@gmail.com>
2024-04-15 21:04:16 -0600

    use LRU for managing the sqlite files

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.22.2
 require (
 	github.com/SaveTheRbtz/zstd-seekable-format-go v0.6.2-0.20231018052958-4410daa6d511
 	github.com/alecthomas/kong v0.9.0
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/georgysavva/scany/v2 v2.1.3
 	github.com/goccy/go-json v0.10.2
+	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/imroc/req/v3 v3.43.2
 	github.com/jaswdr/faker/v2 v2.1.0
 	github.com/jtarchie/sqlitezstd v0.0.0-20240413163231-b4e22906e076
@@ -16,6 +19,7 @@ require (
 	github.com/onsi/ginkgo/v2 v2.17.1
 	github.com/onsi/gomega v1.32.0
 	github.com/phayes/freeport v0.0.0-20220201140144-74d24b5ae9f5
+	github.com/samber/lo v1.39.0
 	github.com/samber/slog-echo v1.12.4
 	github.com/tinylib/msgp v1.1.9
 )
@@ -23,11 +27,8 @@ require (
 require (
 	github.com/SaveTheRbtz/fastcdc-go v0.3.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/benbjohnson/immutable v0.4.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/georgysavva/scany/v2 v2.1.3 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
@@ -45,7 +46,6 @@ require (
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/quic-go v0.42.0 // indirect
 	github.com/refraction-networking/utls v1.6.4 // indirect
-	github.com/samber/lo v1.39.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opentelemetry.io/otel v1.25.0 // indirect
--- managers/local.go
+++ managers/local.go
@@ -7,11 +7,13 @@ import (
 	"regexp"
 
 	"github.com/georgysavva/scany/v2/sqlscan"
+	"github.com/hashicorp/golang-lru/v2/simplelru"
 	filewatcher "github.com/jtarchie/loge/file_watcher"
 	"github.com/samber/lo"
 )
 
 type Local struct {
+	cache     *simplelru.LRU[string, *sql.DB]
 	outputDir string
 	watcher   *filewatcher.FileWatcher
 }
@@ -24,13 +26,25 @@ func NewLocal(
 		return nil, fmt.Errorf("could not start watcher: %w", err)
 	}
 
+	const numCachedDBs = 10
+
+	cache, err := simplelru.NewLRU[string, *sql.DB](numCachedDBs, func(_ string, client *sql.DB) {
+		_ = client.Close()
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not start cache: %w", err)
+	}
+
 	return &Local{
+		cache:     cache,
 		outputDir: outputPath,
 		watcher:   watcher,
 	}, nil
 }
 
 func (m *Local) Close() error {
+	defer m.cache.Purge()
+
 	err := m.watcher.Close()
 	if err != nil {
 		return fmt.Errorf("could close manager: %w", err)
@@ -41,20 +55,21 @@ func (m *Local) Close() error {
 
 func (m *Local) execute(fun func(*sql.DB) error) error {
 	err := m.watcher.Iterate(func(filename string) error {
-		client, err := sql.Open("sqlite3", filename+"?vfs=zstd")
-		if err != nil {
-			return fmt.Errorf("could not open sqlite3 (%q): %w", filename, err)
-		}
-		defer client.Close()
+		client, ok := m.cache.Get(filename)
+		if !ok {
+			var err error
 
-		err = fun(client)
+			client, err = sql.Open("sqlite3", filename+"?vfs=zstd")
+			if err != nil {
+				return fmt.Errorf("could not open sqlite3 (%q): %w", filename, err)
+			}
+
+			m.cache.Add(filename, client)
+		}
+
+		err := fun(client)
 		if err != nil {
 			return fmt.Errorf("could not execute (%q): %w", filename, err)
-		}
-
-		err = client.Close()
-		if err != nil {
-			return fmt.Errorf("could not close connection (%q): %w", filename, err)
 		}
 
 		return nil
